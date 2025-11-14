0f5df00ccaef8deb109d2ce208e6d19386114e20
Delvin233 <yamoahdelvin@gmail.com>
2025-11-14 23:46:41 +0000

    feat: changed font to minimalPro theme

--- packages/nextjs/styles/fontThemes.ts
+++ packages/nextjs/styles/fontThemes.ts
@@ -94,6 +94,6 @@ export const FONT_THEMES: Record<string, FontTheme> = {
 };
 
 // 🎨 CHANGE THIS TO SWITCH THEMES
-export const ACTIVE_FONT_THEME: keyof typeof FONT_THEMES = "cyberpunk";
+export const ACTIVE_FONT_THEME: keyof typeof FONT_THEMES = "minimalPro";
 
 export const getActiveTheme = () => FONT_THEMES[ACTIVE_FONT_THEME];
